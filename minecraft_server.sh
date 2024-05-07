a494fc1dc0393afad50bb60d5e789e863abe8cee
Dave Burt <dave@burt.id.au>
2024-05-07 22:52:11 +1000

    Remove specific old version of java from server start script

--- minecraft_server.sh
+++ minecraft_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Vanilla
-#/usr/lib/jvm/java-19-openjdk-amd64/bin/java -Xmx1024M -Xms1024M -jar minecraft_server.1.19.3.jar nogui
+#java -Xmx1024M -Xms1024M -jar minecraft_server.1.19.3.jar nogui
 
 # Paper
-/usr/lib/jvm/java-19-openjdk-amd64/bin/java -Xmx2G -Xms2G -jar minecraft_server.jar nogui
+java -Xmx2G -Xms2G -jar minecraft_server.jar nogui
 
